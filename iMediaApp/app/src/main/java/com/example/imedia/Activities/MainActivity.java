d348133e7a024e60e22d3b1fae6ee3058b54240c
guiganafe@outlook.com <14*71999>
2020-10-20 13:25:55 -0400

    Navegação e logout concluídos

--- iMediaApp/app/src/main/java/com/example/imedia/Activities/MainActivity.java
+++ iMediaApp/app/src/main/java/com/example/imedia/Activities/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.imedia.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.imedia.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializarComponentes();
+    }
+
+    private void inicializarComponentes() {
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
--- iMediaApp/app/src/main/java/com/example/imedia/Fragments/ProfileFragment.java
+++ iMediaApp/app/src/main/java/com/example/imedia/Fragments/ProfileFragment.java
@@ -1,21 +1,49 @@
 package com.example.imedia.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.imedia.Activities.LogIn;
+import com.example.imedia.Firebase.FirebaseService;
 import com.example.imedia.R;
 
 public class ProfileFragment extends Fragment {
+
+    private Button btn_logout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        inicializarComponentes(view);
+
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseService.logOut();
+                Intent logIn = new Intent(getContext(), LogIn.class);
+                startActivity(logIn);
+                getActivity().finish();
+            }
+        });
+    }
+
+    private void inicializarComponentes(View view) {
+        btn_logout = (Button) view.findViewById(R.id.btn_logout);
     }
 }
